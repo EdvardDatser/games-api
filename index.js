fecc119dcedafb57e72cd570c21c2f5a923f711c
Edvard Datser <edvard.datser@icloud.com>
2024-09-25 14:14:59 +0300

    [SAVE] index.js modified, commit for backup save

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
+app.use(express.json())
+
 const games =[
     {id: 1, name: "Witcher 3", price: 29.99},
     {id: 2, name: "Minecraft", price: 35.99},
-    {id: 3, name: "Valorant", price: 0},
+    {id: 3, name: "Valorant", price: 0.00},
     {id: 4, name: "Satisfactory(zavod)", price: 35.00},
-    {id: 5, name: "Warcraft", price: 0},
-    {id: 6, name: "Roblox", price: 0},
+    {id: 5, name: "Warcraft", price: 0.00},
+    {id: 6, name: "Roblox", price: 0.00},
     {id: 7, name: "Dome keeper", price: 17.99},
 ]
 app.get('/games', (req, res) =>{
@@ -28,4 +31,13 @@ app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port,()=> {
     console.log(`API up at: http://localhost:${port}`)
+})
+
+app.post('/games', (req, res) => {
+  games.push({
+      id: games.lenght + 1,
+      price: req.body.price,
+      name: req.body.name
+  })
+    res.end()
 })
